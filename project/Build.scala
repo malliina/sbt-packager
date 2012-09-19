b3c09f22f6b3242c332c9f236e366c59f4d9f3e7
Michael Skogberg <malliina123@gmail.com>
2012-09-19 21:15:53 +0300

    temp

--- project/Build.scala
+++ project/Build.scala
@@ -3,10 +3,9 @@ import sbt._
 
 object MyBuild extends Build {
 
-  override def settings = super.settings ++ Seq(
-    resolvers += Resolver.url("scalasbt", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns),
-    sbtPlugin := true,
+  override lazy val settings = super.settings ++ Seq(
     name := "sbt-packager",
+    sbtPlugin := true,
     addSbtPlugin("com.typesafe" % "sbt-native-packager" % "0.4.4")
   )
 
