3d481c4748e6fea024d5f7853985dd4d154bb082
Michael Skogberg <malliina123@gmail.com>
2012-09-19 20:17:57 +0300

    cleanup

--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -4,7 +4,7 @@ import sbt._
 object BuildBuild extends Build {
   override lazy val settings = super.settings ++ Seq(
     resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.1.0")
+    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0-SNAPSHOT")
   )
   override def projects = Seq(root)
   lazy val root = Project("build", file("."))
