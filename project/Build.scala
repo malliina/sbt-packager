ebdaeabb1fa784d760bb468806bd880402093de7
Michael Skogberg <malliina123@gmail.com>
2013-01-10 00:17:47 +0100

    2.10, version bump

--- project/Build.scala
+++ project/Build.scala
@@ -4,9 +4,10 @@ import sbt._
 object MyBuild extends Build {
 
   override lazy val settings = super.settings ++ Seq(
+    scalaVersion := "2.10.0",
     organization := "com.mle",
     name := "sbt-packager",
-    version := "0.6-SNAPSHOT",
+    version := "0.7-SNAPSHOT",
     sbtPlugin := true,
     exportJars := true
   )
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -3,8 +3,9 @@ import sbt._
 
 object BuildBuild extends Build {
   override lazy val settings = super.settings ++ Seq(
+    scalaVersion := "2.9.2",
     resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0-SNAPSHOT")
+    addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.3.0-SNAPSHOT")
   )
   override lazy val projects = Seq(root)
   lazy val root = Project("plugins", file("."))
