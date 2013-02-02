b39f2b1360a5e8621770b91c07981158ea5bf343
Michael Skogberg <malliina123@gmail.com>
2013-02-02 21:49:28 +0100

    2.9.2, version down

--- project/Build.scala
+++ project/Build.scala
@@ -2,15 +2,17 @@ import sbt.Keys._
 import sbt._
 
 object MyBuild extends Build {
+  val utilDep = "com.mle" %% "util" % "0.63-SNAPSHOT"
 
   override lazy val settings = super.settings ++ Seq(
-    scalaVersion := "2.10.0",
+    scalaVersion := "2.9.2",
     organization := "com.mle",
     name := "sbt-packager",
-    version := "0.7-SNAPSHOT",
+    version := "0.92-SNAPSHOT",
     sbtPlugin := true,
     exportJars := true
   )
 
   lazy val sbtPackager = Project("sbt-packager", file("."))
+    .settings(libraryDependencies ++= Seq(utilDep))
 }
